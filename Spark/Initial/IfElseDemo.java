5d01ff9fe239129f9866f8e5a39105df21243381
Rajendar Singh <rajendarsingh@Laptop334.local>
2025-06-01 15:07:01 +0530

    into

---
+++ Spark/Initial/IfElseDemo.java
@@ -0,0 +1,14 @@
+public class IfElseDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        if (a > b) {
+            System.out.println("a is greater than b");
+        } else if (a < b) {
+            System.out.println("b is greater than a");
+        } else {
+            System.out.println("a is equal to b");
+        }
+        System.out.println("End of the program");
+    }
+}
---
+++ Spark/Initial/UserInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        scanner.close();
+    }
+}
