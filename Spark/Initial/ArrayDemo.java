7b5ed24a9ca01950c78d38ddfafdbeb188c61e27
Rajendar Singh <rajendarsingh@Laptop334.local>
2025-06-01 15:49:34 +0530

    loops

--- Spark/Initial/ArrayDemo.java
+++ Spark/Initial/ArrayDemo.java
@@ -8,9 +8,28 @@ public class ArrayDemo {
         // arr[2] = 3;
         // arr[3] = 4;
         // arr[4] = 5;
+        // For loop
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 1;
         }
+        // For each loop
+        for (int num : arr) {
+            System.out.println("Number from for each loop: " + num);
+        }
+        // While loop
+        int i = 0;
+        while (i < arr.length) {
+            System.out.println("Number from while loop: " + arr[i]);
+            i++;
+        }
+
+        // Do while loop
+        int j = 0;
+        do {
+            System.out.println("Number from do while loop: " + arr[j]);
+            j++;
+        } while (j < arr.length);
+
         System.out.println(Arrays.toString(arr));
         System.out.println(arr.length);
     }
