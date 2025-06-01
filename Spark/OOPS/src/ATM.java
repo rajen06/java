dbda2bde1cc2bc810c6ade4c48b42fe808fc9432
Rajendar Singh <rajendarsingh@Laptop334.local>
2025-06-01 19:47:34 +0530

    done

---
+++ Spark/OOPS/src/ATM.java
@@ -0,0 +1,20 @@
+import java.security.PublicKey;
+
+public class ATM {
+    private int bal = 1000;
+    private String account = "ATM";
+    private String location = "ATM";
+    private String bankName = "HDFC";
+    private String bankAddress = "HDFC";
+    public static void main(String[] args) {
+
+    }
+
+    public int getBalance() {
+        return bal;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+}
